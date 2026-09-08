db53ce43932928a5e6adc61798a13a5008056286
Xuanyu Lyu <xuanyu.lyu@colorado.edu>
2026-09-08 11:10:17 -0600

    PGS only predictions

--- Scripts/nn_NPE/train_npe_pgs_and_pheno.py
+++ Scripts/nn_NPE/train_npe_pgs_and_pheno.py
@@ -41,10 +41,10 @@ Usage
 -----
     python train_npe_pgs_and_pheno.py \
         --data nn_training_05AMmigration_noVT.csv \
-        --output ./results_npe_unweighted_05AMmigration_noVT2 \
+        --output ./results_npe_unweighted_05AMmigration_noVT2_PGSonly \
         --epochs 500 \
-        --features_file ./results_npe_unweighted_05AMmigration_noVT2/features.txt \
-        --params_file ./results_npe_unweighted_05AMmigration_noVT2/params.txt \
+        --features_file ./results_npe_unweighted_05AMmigration_noVT2_PGSonly/features.txt \
+        --params_file ./results_npe_unweighted_05AMmigration_noVT2_PGSonly/params.txt \
         --pheno_weight_decay 0 \
         --pgs_weight_decay 0
 """
